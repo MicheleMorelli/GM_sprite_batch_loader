ad91ec07f72ae7a36fd6f436edfe5736654fc934
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2018-05-06 13:34:02 +0100

    added doc for decompose

--- decompose_images.sh
+++ decompose_images.sh
@@ -1,4 +1,7 @@
-#! usr/bin/bash
+#! usr/bash
+
+# Uses Imagemagick's coalesce option to decompose a gif into several PNG frames.
+# The frames are then numbered from 0
 
 name=`echo "$1" | cut -d '.' -f1`
 
