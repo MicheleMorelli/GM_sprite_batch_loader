a83c0ac67d88eba0bba8f979d0fb5f177ea729a8
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2018-05-06 13:31:24 +0100

    added doc

--- image_size.sh
+++ image_size.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Gets the size of an image using ImageMagick's identify
+
+# ./image_size.sh [OPTIONS] [IMAGE PATH]
+# OPTIONS:
+# -w   Returns image width
+# -h   Returns image heigth
+
 if [ $# -ne 1 ] 
 then
     [ $1 = '-w' ] && reg='s/^.*? (\d?\d?\d\d)x\d?\d?\d\d .*$/$1/i'
