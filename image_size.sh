3faccab19caf40fb61592628b65a9a767700e2af
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2018-05-06 13:25:51 +0100

    refact

--- image_size.sh
+++ image_size.sh
@@ -2,12 +2,12 @@
 
 if [ $# -ne 1 ] 
 then
-    [ $1 = '-w' ] && str='s/^.*? (\d?\d?\d\d)x\d?\d?\d\d .*$/$1/i'
-    [ $1 = '-h' ] && str='s/^.*? \d?\d?\d\dx(\d?\d?\d\d) .*$/$1/i'
+    [ $1 = '-w' ] && reg='s/^.*? (\d?\d?\d\d)x\d?\d?\d\d .*$/$1/i'
+    [ $1 = '-h' ] && reg='s/^.*? \d?\d?\d\dx(\d?\d?\d\d) .*$/$1/i'
     file=$2 
 else 
     file=$1
-    str='s/^.*? (\d?\d?\d\dx\d?\d?\d\d) .*$/$1/i'
+    reg='s/^.*? (\d?\d?\d\dx\d?\d?\d\d) .*$/$1/i'
 fi
 
-identify $1 | perl -pe "$str"
+identify $1 | perl -pe "$reg"
